cf3c8bb99ccf0024d277d79edd770b3065e57f9c
Vik Borges <v1k@protonmail.com>
2022-07-05 01:38:36 +0100

    todo

--- settings.ts
+++ settings.ts
@@ -1,3 +1,4 @@
+// TODO: more robust user home dir detection. won't work on windows most likely.
 const USER_HOME_PATH = Deno.env.get("HOME");
 
 export const APP_DATA_PATH = `${USER_HOME_PATH}/.deno_daily`;
